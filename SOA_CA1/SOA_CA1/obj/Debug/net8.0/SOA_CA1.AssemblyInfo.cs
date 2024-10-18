a878548a21d7df04706950430f04d865e967b5a9
Matthew-Riddell <d00245674@student.dkit.ie>
2024-10-18 16:52:45 +0100

    Some more CSS

--- SOA_CA1/SOA_CA1/obj/Debug/net8.0/SOA_CA1.AssemblyInfo.cs
+++ SOA_CA1/SOA_CA1/obj/Debug/net8.0/SOA_CA1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68368e5d13018be8e8fea2d12bd662836d783116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89f8513cd2f08cf32605d38d9d07b0c7426a900")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
